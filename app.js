b47b0260857ff161ade47e56f8d28462e833f8b5
guille <guiacosta0000@gmail.com>
2022-04-11 12:09:34 -0300

    Consumiendo api

---
+++ app.js
@@ -0,0 +1,44 @@
+const cards = document.getElementById('card-dinamicas')
+const templateCard = document.getElementById('template-card').content 
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        loadingData(true)
+
+        const res = await fetch('https://rickandmortyapi.com/api/character')
+        //La respuesta la pasamos  a JSON. EN EL FETCH SON DOS AWAIT.
+       const data = await res.json()
+       pintarCard(data)
+    } catch (error) {
+        console.log(error)
+    } finally{
+        loadingData(false)
+    }
+}
+
+const pintarCard = data => {
+    const fragment = document.createDocumentFragment()
+    data.results.forEach( item => {
+        const clone = templateCard.cloneNode(true)
+        clone.querySelector('h5').textContent = item.name 
+        clone.querySelector('p').textContent = item.species
+        clone.querySelector('.card-img-top').setAttribute("src", item.image)
+
+        fragment.appendChild(clone)
+
+    })
+    cards.appendChild(fragment)
+}
+
+const loadingData = estado => {
+    const loading = document.getElementById('loading')
+    if(estado){
+        loading.classList.remove('d-none')
+    } else {
+        loading.classList.add('d-none')
+    }
+}
